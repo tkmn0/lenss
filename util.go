afe1405f099045370927e731332b292c2fa2791e
tkmn0 <tkmgin0914@gmail.com>
2020-11-11 08:23:16 +0900

    reafctor: remove C file struct from go util

--- util.go
+++ util.go
@@ -1,25 +1,10 @@
 package lenss
 
-/*
-#include <stdlib.h>
-#include <stdio.h>
-*/
-import "C"
 import (
 	"image"
 
 	"github.com/tkmn0/lenss/vpx"
 )
-
-type File C.FILE
-
-func OpenFile(filePath string, mode string) *File {
-	return (*File)(C.fopen(C.CString(filePath), C.CString(mode)))
-}
-
-func CloseFile(file *File) {
-	C.fclose((*C.FILE)(file))
-}
 
 func (img *Image) ImageRGBA() *image.RGBA {
 	return (*vpx.Image)(img).ImageRGBA()
